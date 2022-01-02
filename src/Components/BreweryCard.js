16d2804a876eb5345d973247037d9915a194c188
Ivan Akkruatov <jojowoodo@yahoo.com>
2022-01-03 00:30:26 +0200

    removed more unused imports

--- src/Components/BreweryCard.js
+++ src/Components/BreweryCard.js
@@ -1,12 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { useState } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
 import { useNavigate } from "react-router-dom";
 
 const BreweryCard = (props) => {
