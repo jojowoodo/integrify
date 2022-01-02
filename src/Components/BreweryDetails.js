b37b5f45460502211be08c77287b27c6579127a6
Ivan Akkruatov <jojowoodo@yahoo.com>
2022-01-03 00:26:56 +0200

    removed unused imports

--- src/Components/BreweryDetails.js
+++ src/Components/BreweryDetails.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+
 import { useState } from "react";
-import Box from "@mui/material/Box";
+
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
+
 import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Container from "@mui/material/Container";
