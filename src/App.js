ca6c83c2e564052f7caa4e62da0f56c1f96543a3
Ivan Akkruatov <jojowoodo@yahoo.com>
2022-01-02 14:16:30 +0200

    adds basic consts containing the API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [emptyResult, setEmptyResult] = useState(false); // this is the state
+    const [brewery, setBrewery] = useState([]); // array of breweries
+    const [input, setInput] = useState(''); // input from user
+    const [loading, setLoading] = useState(false); // loading state
 
+    const getBreweries = () => {
+      fetch(`https://api.openbrewerydb.org/breweries?by_state=${input}`)
+      .then(res => res.json())
+      .then(data => {
+        setLoading(true);
+        setTimeout(function(){ // this is a timeout function if the array is empty
+          if(data.length < 1) {
+            setEmptyResult(true); // if the array is empty, set the state to true (no results)
+          }
+        setBrewery(data); // set the state to the array of breweries
+        setLoading(false); // set the loading state to false
+        }, 1000);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+};
+
+
+
+}
 export default App;
